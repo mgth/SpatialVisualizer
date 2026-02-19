debadec0dbe32603c82857ab69ba842337410815
Mathieu GRENET <sub@mgth.fr>
2026-02-19 21:29:01 +0100

    Fix browser module loading for OrbitControls

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.165.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js?module';
+import { OrbitControls } from 'https://unpkg.com/three@0.165.0/examples/jsm/controls/OrbitControls.js?module';
 
 const statusEl = document.getElementById('status');
 const layoutSelectEl = document.getElementById('layoutSelect');
